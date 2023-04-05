09462786b31da777099cdb8fd142252f5e5477ae
Martinlsr <127149463+Martinlsr@users.noreply.github.com>
2023-04-05 23:23:48 +0200

    Summary
    Test

---
+++ connexion.js
@@ -0,0 +1,19 @@
+// Récupération des mots de passe depuis le stockage local
+var adminPassword = localStorage.getItem("adminPassword");
+var voterPassword = localStorage.getItem("voterPassword");
+
+// Gestionnaire d'événements pour le bouton "Se connecter"
+document.getElementById("connect-button").addEventListener("click", function() {
+	var password = document.getElementById("password").value;
+
+	if (password === adminPassword) {
+		// Rediriger vers la page d'administration
+		window.location.href = "admin.html";
+	} else if (password === voterPassword) {
+		// Rediriger vers la page de vote
+		window.location.href = "vote.html";
+	} else {
+		// Afficher un message d'erreur si le mot de passe est incorrect
+		alert("Mot de passe incorrect !");
+	}
+});
---
+++ vote.js
@@ -0,0 +1,46 @@
+// Configuration
+var password = "eleve123"; // Mot de passe pour les votants
+var adminPassword = "AdminRoméo2323"; // Mot de passe pour les admins
+
+// Options de vote
+var options = [
+];
+
+// Récupérer le formulaire
+var voteForm = document.getElementById("vote-form");
+
+// Récupérer le select pour les options de vote
+var voteOptionSelect = document.getElementById("vote-option");
+
+// Ajouter les options de vote au select
+for (var i = 0; i < options.length; i++) {
+	var option = options[i];
+	var optionElement = document.createElement("option");
+	optionElement.value = option;
+	optionElement.innerHTML = option;
+	voteOptionSelect.appendChild(optionElement);
+}
+
+// Ajouter un écouteur pour le soumission du formulaire
+voteForm.addEventListener("submit", function(event) {
+	event.preventDefault(); // Empêcher la soumission du formulaire
+	
+	// Récupérer la valeur de l'option de vote choisie
+	var voteOption = voteOptionSelect.value;
+	
+	// Récupérer le mot de passe saisi
+	var votePassword = document.getElementById("vote-password").value;
+	
+	// Vérifier si le mot de passe est correct
+	if (votePassword === password || votePassword === adminPassword) {
+		// Stocker le choix de l'utilisateur dans le localStorage
+		localStorage.setItem("voteOption", voteOption);
+		
+		// Rediriger vers la page de confirmation de vote
+        if (votePassword === adminPassword) {
+            window.location.href = "admin.html";
+        } else {
+            window.location.href = "vote-confirm.html";
+        }
+    }
+});
